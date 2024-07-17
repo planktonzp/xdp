09f702985335a9a1cff22619a99be13ac6c9a422
planktonzp <ruizhipeng001@gmail.com>
2024-07-17 16:08:30 +0800

    <hotfix> xdp: move unix.XDPDesc & unix.XDPStatics to xdp_linux.go

--- xdp.go
+++ xdp.go
@@ -1,5 +1,27 @@
 package xdp
 
+// DefaultSocketFlags are the flags which are passed to bind(2) system call
+// when the XDP socket is bound, possible values include unix.XDP_SHARED_UMEM,
+// unix.XDP_COPY, unix.XDP_ZEROCOPY.
+var DefaultSocketFlags uint16
+
+// DefaultXdpFlags are the flags which are passed when the XDP program is
+// attached to the network link, possible values include
+// unix.XDP_FLAGS_DRV_MODE, unix.XDP_FLAGS_HW_MODE, unix.XDP_FLAGS_SKB_MODE,
+// unix.XDP_FLAGS_UPDATE_IF_NOEXIST.
+var DefaultXdpFlags uint32
+
+// SocketOptions are configuration settings used to bind an XDP socket.
+type SocketOptions struct {
+	NumFrames              int
+	FrameSize              int
+	FillRingNumDescs       int
+	CompletionRingNumDescs int
+	RxRingNumDescs         int
+	TxRingNumDescs         int
+}
+
+
 // DefaultSocketOptions is the default SocketOptions used by an xdp.Socket created without specifying options.
 var DefaultSocketOptions = SocketOptions{
 	NumFrames:              128,
--- xdp_linux.go
+++ xdp_linux.go
@@ -686,16 +686,6 @@ type Socket struct {
 	getTXDescs, getRXDescs   []Desc
 }
 
-// SocketOptions are configuration settings used to bind an XDP socket.
-type SocketOptions struct {
-	NumFrames              int
-	FrameSize              int
-	FillRingNumDescs       int
-	CompletionRingNumDescs int
-	RxRingNumDescs         int
-	TxRingNumDescs         int
-}
-
 // Desc represents an XDP Rx/Tx descriptor.
 type Desc unix.XDPDesc
 
@@ -723,14 +713,3 @@ type Stats struct {
 	// submitted into Fill or Tx ring queues.
 	KernelStats unix.XDPStatistics
 }
-
-// DefaultSocketFlags are the flags which are passed to bind(2) system call
-// when the XDP socket is bound, possible values include unix.XDP_SHARED_UMEM,
-// unix.XDP_COPY, unix.XDP_ZEROCOPY.
-var DefaultSocketFlags uint16
-
-// DefaultXdpFlags are the flags which are passed when the XDP program is
-// attached to the network link, possible values include
-// unix.XDP_FLAGS_DRV_MODE, unix.XDP_FLAGS_HW_MODE, unix.XDP_FLAGS_SKB_MODE,
-// unix.XDP_FLAGS_UPDATE_IF_NOEXIST.
-var DefaultXdpFlags uint32
