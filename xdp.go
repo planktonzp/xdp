ab77757cdf89a661fde55d26eed55d42cb94f301
planktonzp <ruizhipeng001@gmail.com>
2024-07-19 10:04:55 +0800

    <hotfix>XDP_MODE: add work mode to xdp

--- xdp.go
+++ xdp.go
@@ -1,5 +1,12 @@
 package xdp
 
+const (
+	XDP_FLAGS_DRV_MODE          = 0x4
+	XDP_FLAGS_HW_MODE           = 0x8
+	XDP_FLAGS_SKB_MODE          = 0x2
+	XDP_FLAGS_UPDATE_IF_NOEXIST = 0x1
+)
+
 // DefaultSocketFlags are the flags which are passed to bind(2) system call
 // when the XDP socket is bound, possible values include unix.XDP_SHARED_UMEM,
 // unix.XDP_COPY, unix.XDP_ZEROCOPY.
@@ -20,7 +27,6 @@ type SocketOptions struct {
 	RxRingNumDescs         int
 	TxRingNumDescs         int
 }
-
 
 // DefaultSocketOptions is the default SocketOptions used by an xdp.Socket created without specifying options.
 var DefaultSocketOptions = SocketOptions{
